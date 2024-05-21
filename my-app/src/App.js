6a408c3441d1bda56642a858e4dab2be82566940
Gracie Nemusesu <graciekan21@gmail.com>
2024-05-21 16:45:28 +0000

    Added Snippet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 //import "./App.css";
-import UnControlledForm from "./components/UncontrolledForm";
+import { SearchBar} from "./components/SearchBar";
+
 
 function App() {
   return <div className="App">
-      <UnControlledForm />
+      <SearchBar />
   </div>;
 }
 
